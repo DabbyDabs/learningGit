63b5354c9f69691431797e8c4660a842b54ef2a3
Mayank Dabhdya <mayankdabhdya@Mayanks-MacBook-Air.local>
2019-02-11 15:47:02 +0530

    corrections done

--- file1.js
+++ file1.js
@@ -1,6 +1,6 @@
 console.log("this is my file 1");
 console.log("let me try learning about github today")
-momShouter(name){
+function momShouter(name){
     console.log("Howard!"+name+"is on the phone")
     console.log("Howard!"+name+"is on the phone")
     console.log("Howard!"+name+"is on the phone")
