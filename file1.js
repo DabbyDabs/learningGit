d48c9624bd1b74a5961feea608bc7812d1d14e62
Mayank Dabhdya <mayankdabhdya@Mayanks-MacBook-Air.local>
2019-02-11 15:45:25 +0530

    Howard mom function added to the existing leftBranch

--- file1.js
+++ file1.js
@@ -1,2 +1,10 @@
 console.log("this is my file 1");
 console.log("let me try learning about github today")
+momShouter(name){
+    console.log("Howard!"+name+"is on the phone")
+    console.log("Howard!"+name+"is on the phone")
+    console.log("Howard!"+name+"is on the phone")
+
+}
+
+momShouter("mayank");
