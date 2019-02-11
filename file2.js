36915a24ce7818c3b88a2f49b59ea922a416d092
Mayank Dabhdya <mayankdabhdya@Mayanks-MacBook-Air.local>
2019-02-11 15:41:21 +0530

    just checking addition / deletion

--- file2.js
+++ file2.js
@@ -1,3 +1,7 @@
 console.log("my first commit was successful");
 console.log("I am trying to commit the second file now")
-console.log("this is a change that I did afterwards just to check")
+ function justShout(name){
+console.log("I am shouting::"+name);
+}
+justShout("mayank")
+
